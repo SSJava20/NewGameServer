7c5be3873a7b83a9c4bc53dc323efbf0abcf2530
Roman Kostyrko <nubaseg@gmail.com>
2012-04-10 23:26:03 +0300

    add runner

--- src/main/java/com/softserve/gameserver/GameServer.java
+++ src/main/java/com/softserve/gameserver/GameServer.java
@@ -4,26 +4,35 @@
  */
 package com.softserve.gameserver;
 
+import com.softserve.server.SocketServer;
+import com.softserve.server.WebSocketServer;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 /**
  *
  * @author Nubaseg
  */
 public class GameServer {
 
+    private static int DEFAULT_PORT = 3224;
+    private static String DEFAULT_IP = "127.0.0.1";
+
     public static void main(String[] args) {
-//        int port = DEFAULT_PORT;
-//        String ip = DEFAULT_IP;
-//        if (args.length > 1) {
-//            port = Integer.parseInt(args[1]);
-//            ip = args[0];
-//        }
-//        try {
-//            new AppServer(InetAddress.getByName(ip), port);
-//            new WSServer(8080);
-//        } catch (UnknownHostException e) {
-//            e.printStackTrace();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
+        int port = DEFAULT_PORT;
+        String ip = DEFAULT_IP;
+        if (args.length > 1) {
+            port = Integer.parseInt(args[1]);
+            ip = args[0];
+        }
+        try {
+            new SocketServer(InetAddress.getByName(ip), port);
+        } catch (IOException ex) {
+            Logger.getLogger(GameServer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        new WebSocketServer(8080);
+
     }
 }
