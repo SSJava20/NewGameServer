abd91e36da34949a7358c7ba46bdc0c62a194d84
Roman Kostyrko <nubaseg@gmail.com>
2012-04-10 23:17:25 +0300

    ws update1

--- src/main/java/com/softserve/gameserver/GameServer.java
+++ src/main/java/com/softserve/gameserver/GameServer.java
@@ -9,5 +9,21 @@ package com.softserve.gameserver;
  * @author Nubaseg
  */
 public class GameServer {
-    
+
+    public static void main(String[] args) {
+//        int port = DEFAULT_PORT;
+//        String ip = DEFAULT_IP;
+//        if (args.length > 1) {
+//            port = Integer.parseInt(args[1]);
+//            ip = args[0];
+//        }
+//        try {
+//            new AppServer(InetAddress.getByName(ip), port);
+//            new WSServer(8080);
+//        } catch (UnknownHostException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+    }
 }
